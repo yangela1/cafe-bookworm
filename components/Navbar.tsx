2cbba40cebe60e1d84ddff3d641b0d5453147309
Angela Yu <ayu107@my.bcit.ca>
2025-04-17 19:41:29 -0700

    style navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,48 +1,59 @@
 "use client"
+import Link from 'next/link';
 import React from 'react'
+import { navItems, logoName } from '@/data/navItems';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+    const pathname = usePathname();
+
+    const mobileMenu = <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+    >
+        {navItems.map(({ label, href }) => {
+            const isActive = pathname === href;
+            return (
+                <li key={href} className={isActive ? 'underline decoration-solid underline-offset-4 rounded-sm' : ""}>
+                    <Link href={href}>{label}</Link>
+                </li>
+            );
+        })}
+    </ul>;
+
+    const menu = <div role="tablist" className="tabs tabs-border">
+        {navItems.map(({ label, href }) => {
+            const isActive = pathname === href;
+            return (
+                <Link role="tab" key={href} href={href} className={`tab transition-all duration-300 ease-in ${isActive ? "tab-active" : ""}`}>
+                    {label}
+                </Link>
+            );
+        })}
+    </div>;
+
     return (
         <>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar bg-base-100 ">
                 <div className="navbar-start">
+                    {/*mobile drop down*/}
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
+                        {mobileMenu}
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+
+                    {/*logoname*/}
+                    <Link href={logoName.href} className="btn btn-ghost text-xl ">{logoName.label}</Link>
                 </div>
+
+                {/*large nav*/}
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
+                    {menu}
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn btn-accent">unused button</a>
                 </div>
             </div>
         </>
---
+++ data/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    {label:"home", href: "/"},
+    {label:"reviews", href: "/reviews"},
+    {label:"about", href:"/about"}
+]
+export const logoName = {label:"cafe bookworm", href: "/"}
